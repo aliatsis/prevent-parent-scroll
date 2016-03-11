231d9ca5ed4406ccf5075a45d25388e72c8437c3
Anthony Liatsis <anthony.liatsis@gmail.com>
2016-03-10 17:24:44 -0800

    use tape-promise

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,5 +1,8 @@
-import * as test from 'tape';
+import * as tape from 'tape';
 import { Test } from 'tape';
+
+let _test = require('tape-promise');
+const test = _test(tape); // decorate tape 
 
 test('placeholder', function(t: Test): void {
   t.end();
